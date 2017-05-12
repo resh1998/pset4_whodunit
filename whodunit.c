c55847facb34be5acd6c4ff9e66dd4c9592fb5c7
Reshma Kandula <k.reshma1398@gmail.com>
2017-05-12 19:22:32 +0000

    opening input and output files

---
+++ whodunit.c
@@ -0,0 +1,41 @@
+/**
+ * Copies a BMP piece by piece, just because.
+ */
+       
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bmp.h"
+
+int main(int argc, char *argv[])
+{
+    // ensure proper usage
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: ./copy infile outfile\n");
+        return 1;
+    }
+
+    // remember filenames
+    char *infile = argv[1];
+    char *outfile = argv[2];
+
+    // open input file 
+    FILE *inptr = fopen("clue.bmp", "r");
+    if (inptr == NULL)
+    {
+        fprintf(stderr, "Could not open %s.\n", infile);
+        return 2;
+    }
+
+    // open output file
+    FILE *outptr = fopen("verdict.bmp", "w");
+    if (outptr == NULL)
+    {
+        fclose(inptr);
+        fprintf(stderr, "Could not create %s.\n", outfile);
+        return 3;
+    }
+
+    
+}
