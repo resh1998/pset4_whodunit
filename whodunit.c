d3e1b3317057a6f699bf2f6af13603eec35ea2ea
Reshma Kandula <k.reshma1398@gmail.com>
2017-05-12 19:30:17 +0000

    changing colours and writing to outfile

--- whodunit.c
+++ whodunit.c
@@ -76,6 +76,22 @@ int main(int argc, char *argv[])
             // read RGB triple from infile
             fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
             
+            if(triple.rgbtBlue == 0x00 && triple.rgbtGreen == 0x00 && triple.rgbtRed == 0xff)
+            {
+                triple.rgbtBlue = 0xff;
+                triple.rgbtGreen = 0xff;
+                triple.rgbtRed = 0xff;
+            }
+            
+            if( !(triple.rgbtBlue == 0xff && triple.rgbtRed== 0xff && triple.rgbtGreen == 0xff) )
+            {
+                triple.rgbtBlue = 0xff;
+                triple.rgbtGreen = 0x00;
+                triple.rgbtRed = 0x00;
+            }
+            
+            fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+            
         }
     }    
     
